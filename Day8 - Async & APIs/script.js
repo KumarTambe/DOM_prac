151304f0477de804762b8f0da68ff7547efbde36
Kumar Tambe <kumartambe49@gmail.com>
2026-05-30 15:36:12 +0530

    hehe

--- Day8 - Async & APIs/script.js
+++ Day8 - Async & APIs/script.js
@@ -1,28 +1,60 @@
-let inventory = [
-    { name: "Laptop", price: 1000, category: "Tech" },
-    { name: "Headphones", price: 200, category: "Tech" },
-    { name: "Coffee Maker", price: 100, category: "Home" },
-    { name: "Blender", price: 50, category: "Home" }
-];
+// setTimeout function
 
-let shopContainer = document.getElementById("shopContainer");
-let totalDisplay = document.getElementById("totalPrice");
-let input = document.getElementById("searchInput")
+console.log("Burger delivered")
 
-function updateApp(searchText, selectedCategory) {
+setTimeout(() => {
+  console.log("Fries delivered")
+}, 3000) // = 3 sec
 
-    //  STEP 1: FILTER (The Bouncer)
+console.log("Coke delivered")
 
-    let filteredItems = inventory.filter(item => (selectedCategory === "All" || item.category === selectedCategory) && item.name.toLowerCase().includes(searchText.toLowerCase())
+// Promise keyword
 
-  //  STEP 2: REDUCE (The Snowball)
- 
-  let reducedItems = filteredItems.reduce((total, item) => total + item.price, 0)
-  totalDisplay.innerHTML = reducedItems
+let orderCoffee = new Promise((resolve, reject) => {
+  let machineWorking = false;
 
-  //  STEP 3: MAP & JOIN (The Assembly Line)
-  
-  shopContainer.innerHTML = filteredItems.map(item => `<li>${item.name}</li>`).join('')
-  
-  
+  setTimeout(() => {
+    if (machineWorking) {
+      resolve("Here is your Vanilla Latte! ☕");
+    } else {
+      reject("Error: The espresso machine is broken.");
+    }
+  }, 2000);
+});
+
+orderCoffee
+  .then(message => console.log(message))
+  .catch(error => console.log(error));
+
+// but if we had multiple things to wait for then it would get messy as
+// we would need multiple .then and .catch chained together
+
+// therefore we use async/await
+
+// aysnc/await
+
+async function getBreakfast() {
+  try {
+    let message = await orderCoffee();
+    console.log(message)
+  } catch (error) {
+    console.log(error)
+  }
 }
+
+// fetching API and using async/await on it
+
+async function loadUsers() {
+  try {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users")
+    let users = await response.json()
+    console.log(users)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+loadUsers();
+
+// using this in UI 
+
